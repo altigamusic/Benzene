5be9ac7760afa8bef3740047d6cd65556de465a6
Altiga <altigamusic@gmail.com>
2026-01-16 23:32:04 +0200

    Switch os.mkdir to os.makedirs
    The exist_ok flag doesn't exist in mkdir.
    HOW DID THIS WORK??

--- scripts/create_release_build.py
+++ scripts/create_release_build.py
@@ -246,7 +246,7 @@ vec2 fragCoord = gl_FragCoord.xy;
 
 
 def main():
-    os.mkdir("src/generated", exist_ok=True)
+    os.makedirs("src/generated", exist_ok=True)
     config_filename = "config.json"
     output_filename = "src/generated/release.cpp"
     output_header_filename = "src/generated/release_config.h"
